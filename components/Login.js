084e8c6771bcc7e146a0fc7bb8c7c711189b979e
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-01-01 13:56:05 +0000

    login page

---
+++ components/Login.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+function Login() {
+    return (
+        <div className="bg-black relative text-white">
+            <h1>Login screen</h1>
+            <div className="flex flex-col absolute z-50 h-5/6 w-full items-center justify-center space-y-10">
+                <Image className="object-cover " src="https://cdn.discordapp.com/attachments/775345290130489374/926793774343004190/imageedit_25_2278677977.png" width={300} height={300}  />
+                <button className="bg-purple-900 rounded-lg p-3 font-bold animate-pulse">Login to the METAVERSE </button>
+            </div>
+            <div className="w-full h-screen">
+                <Image src="https://cdn.discordapp.com/attachments/775345290130489374/926824039777960056/IMG_7345.jpg"
+                layout="fill" objectFit="cover"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Login
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    images: {
+        domains:["avatars.dicebear.com", "external-content.duckduckgo.com", "cdn.discordapp.com","getwallpapers.com","trumpwallpapers.com","wallpapercave.com"],
+    }
+}
